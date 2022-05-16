d99b12786bfdafa6ce2f2837e39bd1ea82f57a0d
frontend_ming <frontend_ming@163.com>
2022-05-16 18:01:28 +0800

    FIXBUG

--- locale/index.js
+++ locale/index.js
@@ -1,10 +1,14 @@
 import en from './en.json'
 import zhHans from './zh-Hans.json'
 import zhHant from './zh-Hant.json'
-// import ja from './ja.json'
+import ja from './ja.json'
+import kr from './kr.json'
+import sp from './sp.json'
 export default {
 	en,
 	'zh-Hans': zhHans,
 	'zh-Hant': zhHant,
-	// ja
+	ja,
+	kr,
+	sp,
 }
